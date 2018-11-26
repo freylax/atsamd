02fd04606b069251fcaa1bbe100a6b305539cb75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-26 16:49:10 +0000

    Deploy atsamd-rs/atsamd to github.com/atsamd-rs/atsamd.git:gh-pages

---
+++ atsamd21g18a/atsamd21g18a/sercom0/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["i2cm","Register block I2C Master Mode"],["i2cs","Register block I2C Slave Mode"],["spi","Register block SPI Mode"],["usart","Register block USART Mode"]],"struct":[["I2CM","Register block"],["I2CS","Register block"],["SPI","Register block"],["USART","Register block"]],"union":[["BAUD_UNION","USART Baud Rate"],["RegisterBlock","Register block"]]});
